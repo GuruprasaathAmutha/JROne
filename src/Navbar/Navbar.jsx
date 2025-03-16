8a433957dd958b0030a39f4367e7faf8ab2fce3d
GuruprasaathAmutha <guruprasaathamutha@gmail.com>
2025-03-16 09:47:42 +0000

    Sansseriff font added

--- src/Navbar/Navbar.jsx
+++ src/Navbar/Navbar.jsx
@@ -5,7 +5,9 @@ const Navbar = () => {
 
     const [time, setTime] = useState(new Date());
 
-    const [settings, setsettings] = useState(false);
+    const [settings, setsettings] = useState(true);
+
+    // const [isOpen, setIsOpen] = useState(false);
 
 
     useEffect(
@@ -19,18 +21,26 @@ const Navbar = () => {
 
 
     )
-
-
     return (
 
         <div className=' w-full flex justify-between top-2.5 left-2.5 z-50 p-4'>
 
-            <div className='w-7 h-16'>  <img onMouseEnter={() => setsettings(false)} onMouseLeave={() => setsettings(true)} src={`   ${settings ? "src/assets/settings.svg" : "src/assets/settings_normal.svg"} `} alt="An svg settings icon" />
+            <div className='w-7 h-16'>  <img onMouseEnter={() => {
+                setsettings(false);
+                settingsModal();
+            }} onMouseLeave={() => setsettings(true)} src={`${settings ? "src/assets/settings.svg" : "src/assets/settings_normal.svg"} `} alt="An svg settings icon" />
             </div>
-            <div className='Quote font-extrabold text-3xl  text-white font-serif r drop-shadow-[2px_2px_0_black]'>  <p> "JUST DO IT !!" </p></div>
+            <div className='titlefont Quote text-3xl  text-white drop-shadow-[2px_2px_0_black]'>  <p style={{ fontFamily: 'Montserrat' }}> ZEN POMO </p></div>
             <div className='Quote w-32'>  <p className='text-white justify-center font-extrabold '> {time.toUTCString()}</p> </div>
         </div>
     )
+}
+const settingsModal = () => {
+    <div className='bg-violet-400  w-3.5 h-3.5'>
+        <ul>
+            <li>Change background pic</li>
+        </ul>
+    </div>
 }
 
 export default Navbar
